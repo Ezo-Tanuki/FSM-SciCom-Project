f6ee7cceabb4d8e035094bf2c688637cc50af815
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-01-04 17:36:02 +0700

    add printCurrentState

--- main_fsm.c
+++ main_fsm.c
@@ -3,20 +3,24 @@
 void calcState(int* nextState, int* currentState, int* input);
 void calcOutput(int* output, int* currentState);
 void printOutput(int* output);
+void printCurrentState(int* currentState);
 
 int main(void){
     int input2Bit[2];
     int currentState[] = {0, 0};
     int nextState[2];
     int output[2];
+    printCurrentState(currentState);
 
-    while(1){
+    while(1){    
         scanf("%d %d", input2Bit, input2Bit+1);
         calcState(nextState, currentState, input2Bit);
         currentState[0] = nextState[0];
         currentState[1] = nextState[1];
         calcOutput(output, currentState);
+        printf("\n");
         printOutput(output);
+        printCurrentState(currentState);
     }
 
     return 0;
@@ -41,4 +45,8 @@ void printOutput(int* output){
 
     if(output[0]) printf("Please fill up the ink!\n");
     if(output[1]) printf("Please fill up the paper!\n");
+}
+
+void printCurrentState(int* currentState){
+    printf("current state: %d%d\n", currentState[0], currentState[1]);
 }
